d4b7ee91f96599e34b80634b987afa2ad04cf608
Kawsalya Radhakrishnan <79155431+KawsalyaRadhakrishnan@users.noreply.github.com>
2021-03-15 15:08:54 +0530

    Add files via upload

---
+++ Extract subdomain from webpage.py
@@ -0,0 +1,22 @@
+import requests
+import http
+
+domain = input ("Enter a domain:")
+file = open("text.txt")
+content = file.read()
+
+subdomains = content.splitlines()
+
+for subdomain in subdomain:
+    url1 = f"http://{subdomain}.{domain}"
+    url2 = f"https://{subdomain}.{domain}"
+    try:
+        request.get(url1)
+        list = [url1]
+        for i in list:
+             print(i)
+
+        requests.get(url2)
+        list = [url2]
+        for i in list:
+            print(i)
